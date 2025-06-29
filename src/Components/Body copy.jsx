04850e6ce2de508e5eca92b892f694b49e0d3676
Brett Baker <GitBaBB@MyBaBB.com>
2025-06-29 12:50:11 -0700

    took off website

---
+++ src/Components/Body copy.jsx
@@ -0,0 +1,20 @@
+import "./Body.css";
+import YouTubeButton2 from "../Components/YouTube/YouTubeButton2.jsx";
+const Body = () => {
+  return (
+    <>
+    <div className="BodyWrapper">
+      
+    <div className="Body relative flex-row max-h-full min-h-[100px] w-full items-center justify-center whitespace-nowrap
+        font-LibreBaskerville-Italic text-base text-white sm:text-3xl lg:text-4xl">
+     {/* <p> Elsa Rodriguez </p> */}
+     < YouTubeButton2 />
+
+    </div>
+    </div>
+
+    </>
+  );
+};
+
+export default Body;
