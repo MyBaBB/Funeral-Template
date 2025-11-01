77374a243eb780153b919f2ab67b0070f476316e
Brett Baker <gitbabb@mybabb.com>
2025-11-01 11:23:17 -0700

    added music , mamacita

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,40 +1,29 @@
-import { useRef } from "react";
 import "./Body.css";
-import ElsaVanImageButton from '../assets/ElsaImageButton.png';
+import ElsaVanImageButton from '../assets/ElsaImageButton.png' ;
 import ClickTheVan from "./ClickTheVan.jsx";
 import { Link } from "react-router-dom";
-
 const Body = () => {
-  const audioRef = useRef(null);
-
-  const handleClick = () => {
-    setTimeout(() => {
-      if (audioRef.current) {
-      audioRef.current.play().catch((err) => {
-        console.warn("Playback failed:", err);
-      });
-      }
-    }, 30000); // 30-second delay
-  };
-
   return (
     <>
-      {/* Hidden audio element */}
-      <audio
-        ref={audioRef}
-        src="https://images.brainbuddys.com/Sound/Mamacita-song_10.mp3"
-        preload="auto"
-        loop
-      />
+    <Link to="/XList" target="_blank" rel="noopener noreferrer">
+    <div className="BodyWrapper  w-fit">
+
+
+       {/* <div className="Body relative flex max-h-full min-h-[100px] w-screen items-center justify-center whitespace-nowrap
+   ">
+   < CrossImage  />
+    </div> */}
+      
+
 
     <div className="Body relative flex  items-center justify-center w-fit m-auto 
         ">
      <img src={ElsaVanImageButton} alt="Elsa Rodriguez" width={300} height={300} className="rounded-lg m-auto z-10" />
     </div>
-            < ClickTheVan />
+          < ClickTheVan />
     </div>
     
-</a>
+</Link>
     </>
   );
 };
