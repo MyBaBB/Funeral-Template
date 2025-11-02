570074a1b5c76d8321f83249991ce730ef81cf95
Brett Baker <gitbabb@mybabb.com>
2025-11-02 11:54:53 -0800

    changed image name due to cache

--- src/Components/NewBody.jsx
+++ src/Components/NewBody.jsx
@@ -10,7 +10,7 @@ const NewBody = () => {
             <div className="Body2 relative flex items-center justify-center w-fit m-auto"> 
               < PasswordGate correctPassword="Velvet_Cipher" />
     <video   controls className="w-full h-auto">
-      <source src="https://images.brainbuddys.com/Videos/11-2-joypix_security.mp4" type="video/mp4" />
+      <source src="https://images.brainbuddys.com/Videos/11-2-joypix_security12.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
     </div>
