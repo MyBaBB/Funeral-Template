f7f5b774d289f0cc2c08aa9934386caf3dbcb141
Brett Baker <gitbabb@mybabb.com>
2025-11-10 08:17:43 -0800

    made it cooler

--- src/Pages/Zenith.jsx
+++ src/Pages/Zenith.jsx
@@ -1,29 +1,23 @@
-import NewBody from "../Components/NewBody.jsx";
-import { BsFillSafeFill } from "react-icons/bs";
-import Investigator from "/Investigator.png";
+ 
+import Investigator from "/Investigator2.png";
 import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 function App() {
   return (
     <div>
       <div className="ZenithWrapper relative flex h-screen w-full flex-col items-center justify-center bg-[#00100]">
-      <span className="z-10 text-white">
-        Safe & Secure
-         <BsFillSafeFill size={99} color="lime" className="z-10" />
-         
-      </span>
-      
-        <NewBody />
-      </div>
-      <div className="absolute z-50 top-4 left-4">
+       <div className="absolute z-50  ">
        <img src={Investigator} alt="Investigator OK " className="" />
-       <span className="font-Itim-Regular absolute top-[2rem] left-[10rem] -rotate-12 text-green-200 text-4xl">
-        Case Closed
+       <span className="font-Itim-Regular absolute top-[2rem] left-[10rem] -rotate-12 text-[lime] text-4xl">
+        Safe Zone
         <span>
           <IoMdCheckmarkCircleOutline size={48} color="lime" className="inline-block ml-4" />
         </span>
         
         </span>
       </div>
+        
+      </div>
+     
     </div>
   );
 }
