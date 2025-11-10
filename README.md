e451b7fd2d2362a909cbbf5d6829ddf2ba9c95e0
Brett Baker <gitbabb@mybabb.com>
2025-11-10 02:33:41 -0800

    added case closed

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Elsa Rodriquez Memorial Website
+# Private Elsa Rodriquez Memorial Website
 
 ### https://Elsa.BrainBuddys.com
 
  * font-LibreBaskerville-Italic
  * font-Itim-Regular  
 
-![Elsa Rodriquez](public/Funeral350x500Transparency.webp)
+![Elsa Rodriquez](public/Smiley.jpg)
  
 
  
--- src/Pages/Zenith.jsx
+++ src/Pages/Zenith.jsx
@@ -1,6 +1,7 @@
 import NewBody from "../Components/NewBody.jsx";
 import { BsFillSafeFill } from "react-icons/bs";
-
+import Investigator from "/Investigator.png";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 function App() {
   return (
     <div>
@@ -8,9 +9,20 @@ function App() {
       <span className="z-10 text-white">
         Safe & Secure
          <BsFillSafeFill size={99} color="lime" className="z-10" />
+         
       </span>
       
         <NewBody />
+      </div>
+      <div className="absolute z-50 top-4 left-4">
+       <img src={Investigator} alt="Investigator OK " className="" />
+       <span className="font-Itim-Regular absolute top-[2rem] left-[10rem] -rotate-12 text-green-200 text-4xl">
+        Case Closed
+        <span>
+          <IoMdCheckmarkCircleOutline size={48} color="lime" className="inline-block ml-4" />
+        </span>
+        
+        </span>
       </div>
     </div>
   );
